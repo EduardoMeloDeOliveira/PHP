500999c02383e67432258d7d456f639323e537b0
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-10-02 23:38:39 -0300

    finalizado

--- 05 - PHP - Formação Symfony/3 - Symfony o primeiro MVC/controle_series_symfony/src/Controller/SeriesController.php
+++ 05 - PHP - Formação Symfony/3 - Symfony o primeiro MVC/controle_series_symfony/src/Controller/SeriesController.php
@@ -2,23 +2,27 @@
 
 namespace App\Controller;
 
-use http\Env\Request;
+use App\Entity\Series;
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SeriesController extends AbstractController
 {
+
+    public function __construct(private SeriesRepository $repository)
+    {
+
+    }
+
     #[Route('/', name: 'app_series')]
     public function index(): Response
     {
-        $serieList = [
-            "Dois homens e meio",
-            "Limtless",
-            "Game of thrones",
-            "House of dragons"
-        ];
+        $serieList = $this->repository->findAll();
 
 //        return new JsonResponse($serieList);
 
@@ -31,12 +35,25 @@ class SeriesController extends AbstractController
     }
 
     #[Route('/series/create', methods: ['GET'])]
-    public function form():Response
+    public function form(): Response
     {
 
-      return $this->render("series/form.html.twig" , [
-          "controller_name" => "Adicione uma nova série",
-      ]);
+        return $this->render("series/form.html.twig", [
+            "controller_name" => "Adicione uma nova série",
+        ]);
 
+    }
+
+
+    #[Route('/series/create', methods: ['POST'])]
+    public function addSerie(Request $request): Response
+    {
+        $getFromInput = $request->request->get('seriesName');
+        $serie = new Series($getFromInput);
+
+
+        $this->repository->save($serie,true);
+
+        return new RedirectResponse("/", 201);
     }
 }
