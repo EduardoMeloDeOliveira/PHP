7b0290bf69e1cda9719410c050e2ed314b0eb4e1
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-10-07 00:53:55 -0300

    adiciona logica de cache pra otimizar queries

--- 05 - PHP - Formação Symfony/5 - Symfony cache e segurança/controle_series_symfony/src/Controller/SeasonsController.php
+++ 05 - PHP - Formação Symfony/5 - Symfony cache e segurança/controle_series_symfony/src/Controller/SeasonsController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use App\Entity\Series;
 use App\Repository\SeasonRepository;
 use App\Repository\SeriesRepository;
+use Doctrine\ORM\PersistentCollection;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+use Twig\Cache\CacheInterface;
 
 class SeasonsController extends AbstractController
 {
 
-    public function __construct(private SeasonRepository $seasonRepository)
+    public function __construct(
+        private SeasonRepository $seasonRepository,
+        private CacheItemPoolInterface $cache)
     {
 
     }
@@ -20,11 +28,22 @@ class SeasonsController extends AbstractController
     #[Route('/seasons/{series}', name: 'app_seasons_redirect_info')]
     public function index(Series $series): Response
     {
+        $season = $this->cache->get(
+            "series_{$series->getId()}_season",
+            function (ItemInterface $item) use ($series) {
+                $item->expiresAfter(5);
 
-        $season = $series->getSeason();
+                /** @var PersistentCollection $season */
+                $season = $series->getSeason();
+                $season->initialize();
+
+                return $season;
+            }
+        );
+
 
         return $this->render('seasons/index.html.twig', [
-            'controller_name' => "Temporadas da série: {$series->getName()}" ,
+            'controller_name' => "Temporadas da série: {$series->getName()}",
             'seasons' => $season,
             'series_name' => $series->getName()
         ]);
